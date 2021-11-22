43e81843e31484de45973b89fe4f22661c87ac3a
danaivach <danai.vacht@gmail.com>
2021-11-22 19:35:50 +0100

    Update to leubot v1.3.0

--- src/main/java/TDGenerator.java
+++ src/main/java/TDGenerator.java
@@ -48,7 +48,7 @@ public class TDGenerator {
 
         Hoverbot hoverBot = new Hoverbot("http://10.10.10.105/", "urn:hoverbot_garuda", "hoverbot");
 
-        PhantomXReactor leubot1 = new PhantomXReactor("https://api.interactions.ics.unisg.ch/leubot1/v1.2/",
+        PhantomXReactor leubot1 = new PhantomXReactor("https://api.interactions.ics.unisg.ch/leubot1/v1.3.0/",
                 "http://yggdrasil.interactions.ics.unisg.ch/environments/61/workspaces/102/artifacts/leubot1", "leubot1");
 
         PhantomXReactor leubot2 = new PhantomXReactor("https://api.interactions.ics.unisg.ch/leubot2/v1.2/",
